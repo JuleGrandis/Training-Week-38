d048ea9ca00a160341f88d2d583c839b721b4980
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2024-09-16 13:56:31 +0200

    Completed Task E and F

--- wks.mjs
+++ wks.mjs
@@ -16,7 +16,7 @@ console.log(waterJuiceRatio);
 console.log(daysUntilBirthday);
 console.log(millimetersOfRain);
 //#endregion
-
+addSpacing(1);
 //#region Task B
 console.log("Task B:");
 
@@ -26,7 +26,7 @@ const minutesIn123Days = HOURS_A_DAY * MINUTES_IN_HOUR * 123;
 console.log(secondsIn2_5hours);
 console.log(minutesIn123Days);
 //#endregion
-
+addSpacing(1);
 //#region Task C
 console.log("Task C:");
 
@@ -34,11 +34,38 @@ for (let i=1; i <= 10; i++) {
     console.log(i);
 }
 //#endregion
-
+addSpacing(1);
 //#region Task D
 console.log("Task D:");
 
 for (let i = 10; i >= 1; i--) {
     console.log(i);
+}
+//#endregion
+addSpacing(1);
+//#region Task E
+console.log("Task E:");
+
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
+//#endregion
+addSpacing(1);
+//#region Task F
+console.log("Task F:")
+
+let i = 0;
+while (i <= 100) {
+    console.log(i);
+    i++;
+}
+//#endregion
+addSpacing(1);
+
+//#region Util Functions
+function addSpacing(lines = 1) {
+    for (let i = 0; i < lines; i++){
+        console.log("")
+    }
 }
 //#endregion
