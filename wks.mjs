1c5deeda5d98f2743851efbb380407b8d3e76b73
JuleGrandis <AleksanderNordrumBuseth@gmail.com>
2024-09-16 13:45:06 +0200

    Completed Task A and B

---
+++ wks.mjs
@@ -0,0 +1,27 @@
+//#region Task A
+console.log("Task A:");
+
+const HOURS_A_DAY = 24;
+const MINUTES_IN_HOUR = 60;
+const SECONDS_IN_MINUTE = 60;
+
+let waterJuiceRatio = 3;
+let daysUntilBirthday = 7;
+let millimetersOfRain = 5;
+
+console.log(HOURS_A_DAY);
+console.log(MINUTES_IN_HOUR);
+console.log(SECONDS_IN_MINUTE);
+console.log(waterJuiceRatio);
+console.log(daysUntilBirthday);
+console.log(millimetersOfRain);
+//#endregion
+
+//#region Task B
+console.log("Task B:");
+
+const secondsIn2_5hours = MINUTES_IN_HOUR * SECONDS_IN_MINUTE *2.5;
+const minutesIn123Days = HOURS_A_DAY * MINUTES_IN_HOUR * 123;
+
+console.log(secondsIn2_5hours);
+console.log(minutesIn123Days);
